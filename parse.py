3838dad5cd2ef0e7922c55d0a0e49958a2c6f709
ssparashar <sahilsinghparashar@gmail.com>
2021-09-23 08:19:11 +0000

    observed the failing case of pikachu where the language en was not 0th index, worked on a fix, more work needed

--- parse.py
+++ parse.py
@@ -4,7 +4,7 @@ import requests
 from urllib.request import urlopen
 
 
-r = requests.get('https://pokeapi.co/api/v2/pokemon-species/crobat/')
+r = requests.get('https://pokeapi.co/api/v2/pokemon-species/pikachu/')
 
 #with urlopen("https://pokeapi.co/api/v2/pokemon-species/jigglypuff/") as response:
 #    source = response.read()
@@ -13,12 +13,16 @@ r_dict = r.json()
 
 pokemon = dict()
 pokemon['name'] = r_dict['name']
-pokemon['description'] = r_dict['flavor_text_entries'][1]['flavor_text'].replace("\n", " ").replace("\x0c", " ")
+for k in r_dict['flavor_text_entries']:
+    if k['language']['name'] == "en":
+        val = r_dict['flavor_text_entries'].index(k)
+        print(val)
+pokemon['description'] = r_dict['flavor_text_entries'][val]['flavor_text'].replace("\n", " ").replace("\x0c", " ")
 pokemon['habitat'] = r_dict['habitat']['name']
 pokemon['is_legendary'] = r_dict['is_legendary']
 
 print(pokemon)
-n =  r_dict['flavor_text_entries'][1]['flavor_text'].replace("\n", " ").replace("\x0c", " ")
+#n =  r_dict['flavor_text_entries'][1]['flavor_text'].replace("\n", " ").replace("\x0c", " ")
 
 f = json.dumps(pokemon, indent =4)
 
@@ -26,7 +30,7 @@ print(f)
 j= {'text':n}
 #n = " ".join(char for char in n if unicodedata.category(char)[0]!="C")
 
-print(n)
+#print(n)
 print(r_dict['habitat']['name'])
 #print(type(n))
 #print(j)
@@ -36,9 +40,11 @@ if r_dict['habitat']['name'] == "cave" or r_dict['is_legendary'] == True:
     res = requests.post('https://api.funtranslations.com/translate/yoda.json', data=j)
     fin = res.json()
     print(res.status_code)
+    print(fin['error']['message'])
     #print("\n")
     #print(fin)
     print(fin['contents']['translated'])
+    #print(res.text)
 
 else:
     res = requests.post('https://api.funtranslations.com/translate/shakespeare.json', data=j)
